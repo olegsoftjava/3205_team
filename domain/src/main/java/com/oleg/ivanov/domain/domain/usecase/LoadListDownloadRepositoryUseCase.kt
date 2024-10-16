767a35dfdda7760ea9ab6a5ceb764cdc43c65406
ivansoft.by@gmail.com <23101979Tt>
2024-10-16 19:40:25 +0200

    Start

--- domain/src/main/java/com/oleg/ivanov/domain/domain/usecase/LoadListDownloadRepositoryUseCase.kt
+++ domain/src/main/java/com/oleg/ivanov/domain/domain/usecase/LoadListDownloadRepositoryUseCase.kt
@@ -1,10 +1,7 @@
 package com.oleg.ivanov.domain.domain.usecase
 
-import com.oleg.ivanov.domain.domain.data.DownloadLinkAndFileDomain
 import com.oleg.ivanov.domain.domain.repository.RepositoryData
 
 class LoadListDownloadRepositoryUseCase(private val repositoryData: RepositoryData) {
-    suspend fun execute(): List<DownloadLinkAndFileDomain>? {
-        return repositoryData.getDataFromCache()
-    }
+    suspend fun execute() = repositoryData.getDataFromCache()
 }
